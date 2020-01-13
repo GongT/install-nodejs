516f6ad9bd9f9be3ea9d4ac249cafefaf09d0643
GongT <admin@gongt.me>
2020-01-13 11:59:39 +0800

    add yarn and pnpm

--- install.sh
+++ install.sh
@@ -51,7 +51,7 @@ if [ ${OLD_EXISTS} -eq 1 ]; then
 		unlink latest-nodejs.txt
 		echo "official node.js not updated:"
 		echo "  current version: $(/usr/nodejs/bin/node -v)"
-		exit 0
+		# exit 0
 	fi
 fi
 
@@ -59,7 +59,7 @@ echo "installing..."
 echo "	downloading file: https://nodejs.org/dist/latest/${NODE_PACKAGE}"
 wget --continue https://nodejs.org/dist/latest/${NODE_PACKAGE} --progress=bar:force -O nodejs.tar.xz
 
-unlink latest-nodejs.txt
+unlink latest-nodejs.txt || true
 
 if [ -e "nodejs-install-temp-dir" ]; then
 	rm -rf nodejs-install-temp-dir
@@ -90,6 +90,9 @@ else
 	exit 1
 fi
 
+echo 'export PATH="$PATH:/usr/nodejs/bin:/usr/nodejs/yarn/bin:./node_modules/.bin"' > /etc/profile.d/nodejs.sh
+source /etc/profile.d/nodejs.sh
+
 function install {
 	local ITEMS=()
 	local i
@@ -108,18 +111,41 @@ function install {
 		fi
 	done
 	
-	npm rebuild "${ITEMS[@]}"
+	/usr/nodejs/bin/npm rebuild "${ITEMS[@]}"
 }
 if [ "${OLD_EXISTS}" ]; then
+	echo "rebuild global node_modules folder..."
 	PREFIX=$(/usr/nodejs/bin/npm config --global get prefix)
 	cd "${PREFIX}/lib/node_modules"
 	
 	install || die "nodejs install success. But can not rebuild some global packages. This may or may not cause error."
 fi
 
-echo "nodejs install success."
+wget https://yarnpkg.com/latest-rc.tar.gz -O /tmp/install-yarn.tar.gz --progress=bar:force --continue
+rm -rf /usr/nodejs/yarn
+mkdir -p /usr/nodejs/yarn
+tar -zvxf /tmp/install-yarn.tar.gz -C /usr/nodejs/yarn --strip-components=1
 
-if ! command -v node &>/dev/null; then
-	echo 'export PATH="$PATH:/usr/nodejs/bin"' > /etc/profile.d/nodejs.sh
-	echo 'you must add /usr/nodejs/bin to your $PATH'
+echo -n "  yarn: "
+/usr/nodejs/yarn/bin/yarn -v
+
+mkdir -p /usr/nodejs/etc || true
+[[ -e /usr/nodejs/etc/yarnrc ]] || touch /usr/nodejs/etc/yarnrc || true
+[[ -e /usr/nodejs/etc/npmrc ]] || touch /usr/nodejs/etc/npmrc || true
+
+if ! grep 'global-folder' /usr/nodejs/etc/yarnrc -q ; then
+	echo '' >> /usr/nodejs/etc/yarnrc
+	echo 'global-folder "/usr/nodejs/lib"' >> /usr/nodejs/etc/yarnrc
 fi
+
+if command -v unpm &>/dev/null ; then
+	echo "Installing package manager: unpm"
+	yarn global add @idlebox/package-manager || echo "Failed to install @idlebox/package-manager. that is not fatal."
+fi
+if command -v pnpm &>/dev/null ; then
+	echo "Installing package manager: pnpm"
+	yarn global add pnpm || echo "Failed to install pnpm. that is not fatal."
+fi
+
+echo "nodejs install success."
+echo 'You should run "source /etc/profile.d/nodejs.sh" or restart current session to take effect.'
